0961f3c3eef28d44a0d117146eb00132ca7fc040
Greg Courville <greg.courville@czbiohub.org>
2024-06-13 22:09:45 -0700

    migrate setup metadata to pyproject.toml

--- driver/setup.py
+++ driver/setup.py
@@ -1,10 +1,4 @@
 import setuptools
 
 
-setuptools.setup(
-     name="uxibxx",
-     version="0.1.0",
-     python_requires=">=3.6",
-     install_requires=['pyserial'],
-     py_modules=["uxibxx"],
-     )
+setuptools.setup()
